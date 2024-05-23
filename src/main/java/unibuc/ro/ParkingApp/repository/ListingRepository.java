e45aebfbb2f9e8bd6af60ecff387c4b9e4dbd1f3
Radu Mocanu <radu.mocanu1@s.unibuc.ro>
2024-05-24 00:23:46 +0300

    added part of advance filtering logic

--- src/main/java/unibuc/ro/ParkingApp/repository/ListingRepository.java
+++ src/main/java/unibuc/ro/ParkingApp/repository/ListingRepository.java
@@ -14,18 +14,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ListingRepository extends JpaRepository<Listing, UUID> {
-    @Query("SELECT l FROM Listing l WHERE \n" +
-            "    (:sector IS NULL OR l.sector = :sector) \n" +
-            "    AND (:startDate IS NULL OR :endDate IS NULL OR l.startDate >= :startDate AND (l.endDate IS NULL OR l.endDate <= :endDate))\n" +
-            "    AND (:maxDailyPrice IS NULL OR l.price <= :maxDailyPrice)\n" +
-            "    AND (:maxMonthlyPrice IS NULL OR l.monthlyPrice <= :maxMonthlyPrice)\n" +
-            "    AND (:indefinitePeriod = false OR (l.longTermRent = true AND (:sector IS NULL OR l.sector = :sector)))\n   ")
-    List<Listing> findListingsByFilters(@Param("sector") Integer sector,
-                                        @Param("startDate") Date startDate,
-                                        @Param("endDate") Date endDate,
-                                        @Param("maxDailyPrice") Integer maxDailyPrice,
-                                        @Param("maxMonthlyPrice") Integer maxMonthlyPrice,
-                                        @Param("indefinitePeriod") boolean indefinitePeriod);
-
+public interface ListingRepository extends JpaRepository<Listing, UUID> , ListingRepositoryCustom{
 }
